fc9861091dd2d992707a0a8c3055078be82e0e0f
Rebecca <rebj@Rebeccas-MacBook-Pro.local>
2021-09-13 12:46:13 -0400

    d

--- vue.config.js
+++ vue.config.js
@@ -5,8 +5,8 @@ var desktop = "desktop";
 
 module.exports = {
   outputDir: path.resolve(__dirname, desktop),
-  // publicPath: process.env.NODE_ENV === "production" ? `./${desktop}/` : "",
-  publicPath: process.env.NODE_ENV === "production" ? "./" : "",
+  publicPath: process.env.NODE_ENV === "production" ? `./${desktop}/` : "",
+  // publicPath: process.env.NODE_ENV === "production" ? "./" : "",
   assetsDir: assetsPath,
   lintOnSave: true,
   chainWebpack: (config) => {
